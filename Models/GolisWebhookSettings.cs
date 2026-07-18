c028bed03c9e7318de869eee7bed02150f406beb
saed habeb <scxabeeb@gmail.com>
2026-07-18 10:27:17 +0300

    Fix build: add missing GolisWebhookSettings model

---
+++ Models/GolisWebhookSettings.cs
@@ -0,0 +1,8 @@
+namespace VehicleTax.Web.Models;
+
+public class GolisWebhookSettings
+{
+    public string Secret { get; set; } = string.Empty;
+    public string ApiUsername { get; set; } = string.Empty;
+    public string ApiPassword { get; set; } = string.Empty;
+}
