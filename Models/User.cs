3849130f347b16e102858fc4d768a4e16f4f473d
saed habeb <scxabeeb@gmail.com>
2026-08-15 01:59:39 +0300

    feat: add FullName/Phone to User model, update UsersReport, add icon to checkpoint report, reorder menu with Operations Reports first and Revenue & Audit last, professional redesign

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,10 @@ public class User
 
     public string Username { get; set; } = "";
 
+    public string FullName { get; set; } = "";
+
+    public string Phone { get; set; } = "";
+
     public string Password { get; set; } = "";
 
     public string Role { get; set; } = "Collector";
